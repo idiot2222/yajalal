ee5d701641b68031a6fe387f12a567a80d9710a4
idiot2222 <pky2892@gmail.com>
2022-06-07 23:24:16 +0900

    modify mapper
    mapper에 메서드 몇 개 추가함

--- infrastructure/jpa/src/main/java/me/bogeun/mapper/PlayerMapper.java
+++ infrastructure/jpa/src/main/java/me/bogeun/mapper/PlayerMapper.java
@@ -1,12 +1,19 @@
 package me.bogeun.mapper;
 
 import me.bogeun.domain.Player;
+import me.bogeun.entity.PlayerEntity;
 import me.bogeun.payload.player.PlayerCreateDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PlayerMapper {
 
-    Player createDtoToPlayer(PlayerCreateDto createDto);
+    @Mapping(target = "id", ignore = true)
+    PlayerEntity createDtoToPlayerEntity(PlayerCreateDto createDto);
+
+    Player entityToPlayer(PlayerEntity entity);
+
+    PlayerEntity playerToEntity(Player player);
 
 }
--- infrastructure/jpa/src/main/java/me/bogeun/mapper/UserMapper.java
+++ infrastructure/jpa/src/main/java/me/bogeun/mapper/UserMapper.java
@@ -1,12 +1,18 @@
 package me.bogeun.mapper;
 
 import me.bogeun.domain.User;
+import me.bogeun.entity.UserEntity;
 import me.bogeun.payload.user.UserCreateDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "id", ignore = true)
     User createDtoToUser(UserCreateDto createDto);
 
+    UserEntity userToEntity(User user);
+
+    User entityToUser(UserEntity entity);
 }
