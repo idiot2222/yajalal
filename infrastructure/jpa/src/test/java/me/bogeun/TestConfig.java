1d3d6f1542bef51acd6d6f4e72e2d06451c61314
idiot2222 <pky2892@gmail.com>
2022-06-10 12:03:25 +0900

    modify application.properties
    스프링 부트에서 application.properties 파일은 빌드 될 때 하나만 만들어질 수 있다고 한다. 그래서 여러개 만들면 하나만 사용되고 나머지는 무시되더라.
    여러 개 등록을 하려면 @PropertySources 사용해서 할 수 있고, 그럼 알아서 빌드 될 때는 하나로 합친단다.
    또, @PropertySources 는 아직 .yml은 지원 안해서 인식이 안되길래 .properties로 다 바꿈.
    속성 파일을 하나로 합쳐서 만드는 방법도 있었지만, 내는 여러개 각자 만들고 싶어서 boot쪽에 config 하나 만들어서 각자 만든걸 추가하는 방식으로 함. 테스트는 따로 만들어서 따로 TestConfig에 등록해서 사용함.

--- infrastructure/jpa/src/test/java/me/bogeun/TestConfig.java
+++ infrastructure/jpa/src/test/java/me/bogeun/TestConfig.java
@@ -3,6 +3,7 @@ package me.bogeun;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @TestConfiguration
 // 얘는 @Component 붙은 애들 찾아서 빈으로 등록하는 애
@@ -11,5 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
 // 프로젝트에 드간 의존성을 보고 해당되는 설정을 자동으로 사용하여 빈을 등록시켜주는 애
 // 내 같은 경우는 HibernateJpaAutoConfiguration 얘 때문에 이거 썼는데, 얘만 따로 할 수 있는 방법은 없나
 @EnableAutoConfiguration
+@PropertySource("classpath:application-jpa-test.properties")
 public class TestConfig {
 }
