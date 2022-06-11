390d4fff18815ba340c3cd95b1be8ea88b92e04b
idiot2222 <pky2892@gmail.com>
2022-06-11 21:02:43 +0900

    create UserController

---
+++ presentation/api/src/main/java/me/bogeun/controller/UserController.java
@@ -0,0 +1,46 @@
+package me.bogeun.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.bogeun.domain.User;
+import me.bogeun.payload.user.UserCreateDto;
+import me.bogeun.payload.user.UserUpdateDto;
+import me.bogeun.port.incoming.UserServicePort;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserServicePort userServicePort;
+
+    @PostMapping("/join")
+    public String userJoin(@RequestBody UserCreateDto createDto) {
+        userServicePort.createUser(createDto);
+
+        return "ok";
+    }
+
+    @GetMapping("/info/{userId}")
+    public User userInfo(@PathVariable Long userId) {
+        return userServicePort.getUserById(userId);
+    }
+
+    @PostMapping("/update/{userId}")
+    public User updateUserInfo(@PathVariable Long userId, UserUpdateDto updateDto) {
+        updateDto.setId(userId);
+
+        return userServicePort.updateUser(updateDto);
+    }
+
+    @PostMapping("/delete/{userId}")
+    public String deleteUser(@PathVariable Long userId) {
+        try {
+            userServicePort.deleteUser(userId);
+        } catch (IllegalArgumentException e) {
+            return "error";
+        }
+
+        return "ok";
+    }
+}
