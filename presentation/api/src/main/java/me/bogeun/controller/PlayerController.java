a2ed6f6a63ce2961df03e843d368f72a0b5fa8ff
idiot2222 <pky2892@gmail.com>
2022-06-21 12:18:37 +0900

    add Controller
    team이랑 player

---
+++ presentation/api/src/main/java/me/bogeun/controller/PlayerController.java
@@ -0,0 +1,46 @@
+package me.bogeun.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.bogeun.domain.Player;
+import me.bogeun.payload.player.PlayerCreateDto;
+import me.bogeun.payload.player.PlayerUpdateDto;
+import me.bogeun.port.incoming.PlayerServicePort;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+
+    private final PlayerServicePort playerServicePort;
+
+    @PostMapping("/create/{userId}")
+    public String createPlayer(@PathVariable Long userId,
+                               @RequestBody PlayerCreateDto createDto) {
+        playerServicePort.createPlayer(createDto, userId);
+
+        return "ok";
+    }
+
+    @GetMapping("/info/{playerId}")
+    public Player playerInfo(@PathVariable Long playerId) {
+        return playerServicePort.getPlayerById(playerId);
+    }
+
+    @PostMapping("/update/{playerId}")
+    public Player updatePlayerInfo(@PathVariable Long playerId,
+                                   @RequestBody PlayerUpdateDto updateDto) {
+        return playerServicePort.updatePlayer(updateDto, playerId);
+    }
+
+    @PostMapping("/delete/{playerId}")
+    public String deletePlayer(@PathVariable Long playerId) {
+        try {
+            playerServicePort.deletePlayer(playerId);
+        } catch (IllegalArgumentException e) {
+            return "error";
+        }
+
+        return "ok";
+    }
+}
